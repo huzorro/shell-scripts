7a78257fff7eae76dd8ceb7e479533496d54da59
linuxme <linuxme@126.com>
2013-11-19 21:20:35 +0800

    Update switch_mysqlslowlog_to_csv.sh
    add usage

--- system-management/switch_mysqlslowlog_to_csv.sh
+++ system-management/switch_mysqlslowlog_to_csv.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#使用： bash switch_mysqllog.sh source_slowlog_file  dest_txt_file
+#           用excel打开dest_txt_file，以‘|’为分割符，另存为csv格式即可
+ 
+#注意：中文乱码问题
+#         先用记事本打开，另存编码为ANSI即可
+
+
 echo "time|user|ip|query_time|lock_time|rows_sent|rows_examined|timestamp|sql" >> $2
 nawk 'BEGIN{OFS="|"}
 /# Time:/{
